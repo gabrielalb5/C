ca595e6d67b7e50336a8c3652d34eabd9e94c1da
Gabriel Albino <82725552+gabrielalb5@users.noreply.github.com>
2024-07-17 21:25:09 -0300

    Add files via upload

--- IPCO/07-15/ex8.c
+++ IPCO/07-15/ex8.c
@@ -2,8 +2,38 @@
 #include <string.h>
 #define TAM 50
 
-void lerVetor();
+void lerVetor(char texto[TAM]);
+void substituir(char texto[TAM], char l1[1], char l2[1]);
+void imprimir(char texto[TAM]);
 
 int main(){
-    
+    char texto[TAM], l1[1], l2[1];
+
+    printf("Digite L1: ");
+    scanf("%s",l1);
+    printf("Digite L2: ");
+    scanf("%s",l2);
+    lerVetor(texto);
+    substituir(texto,l1,l2);
+    imprimir(texto);
+
+}
+
+void lerVetor(char texto[TAM]){
+    printf("Digite a palavra: ");
+    scanf("%s",texto);
+}
+
+void substituir(char texto[TAM], char l1[1], char l2[1]){
+    int i;
+    for(i=0;i<TAM;i++){
+        if(texto[i]==l1[0]){
+            texto[i]==l2[0];
+        }
+    }
+    printf("%c",l1[0]);
+    printf("%c",l2[0]);
+}
+void imprimir(char texto[TAM]){
+    printf("Palavra: %s",texto);
 }
