bf591f0bc5c700154e87ebfd7139f8de7f1ca449
gabrielalb5 <gabrielalbino28@gmail.com>
2024-08-09 11:28:20 -0300

    c

---
+++ IPCO/08-05/JOGOVELHA.c
@@ -0,0 +1,110 @@
+/* Um exemplo de jogo da velha simples */
+#include <stdio.h>
+#include <stdlib.h>
+
+char matrix[3][3]; /* a matriz do jogo */
+
+char check(void);
+void init_matrix(void);
+void get_player_move(void);
+void get_computer_move(void);
+void disp_matrix(void);
+
+int main(void){
+  char done;
+  printf("Jogo da velha.\n");
+  printf("Voce jogara contra o computador.\n");
+  done = ' ';
+  init_matrix();
+  do {
+    disp_matrix();
+    get_player_move();
+    done = check();  /* verifica se ha vencedor */
+    if(done!=' ') break;  /* vencedor! */
+    get_computer_move();
+    done = check();  /* verifica se ha vencedor */
+  } while(done==' ');
+  disp_matrix();  /* mostra as posicoes finais */
+  if(done=='X') printf("\n Humano ganhou! \n");
+  else printf("\n Computador ganhou! \n");
+  return 0;
+}
+
+/* Inicializa a matriz */
+void init_matrix(void)
+{
+  int i, j;
+
+  for(i=0; i<3; i++)
+     for(j=0; j<3; j++) matrix[i][j] = ' ';
+}
+
+/* Obtem a sua jogada */
+void get_player_move(void)
+{
+  int x, y;
+
+  printf("Entre com as coordenadas para o X: ");
+  scanf("%d%d", &x, &y);
+  x--; y--;
+  if(matrix[x][y]!=' ') {
+    printf("Posicao invalida, tente novamente. \n");
+    get_player_move();
+  }
+  else matrix[x][y] = 'X';
+}
+
+/* Obtem uma jogada do computador */
+void get_computer_move(void)
+{
+  int i, j;
+
+  for(i=0; i<3; i++) {
+    for(j=0; j<3; j++)
+      if(matrix[i][j]==' ') break;
+    if(matrix[i][j]==' ') break;
+  }
+  if(i*j==9) {
+    printf("empate\n");
+    exit(0);
+  }
+  else
+    matrix[i][j] = 'O';
+}
+
+/* Mostra a matriz na tela */
+void disp_matrix(void)
+{
+  int t;
+
+  for(t=0; t<3; t++) {
+    printf(" %c | %c | %c ", matrix[t][0], matrix[t][1],
+         matrix[t][2]);
+    if(t!=2) printf("\n---|---|---\n");
+  }
+  printf("\n");
+}
+
+/* Verifica se ha um vencedor */
+char check(void)
+{
+  int i;
+
+  for(i=0; i<3; i++) /* verifica as linhas */
+    if(matrix[i][0]==matrix[i][1] &&
+       matrix[i][0]==matrix[i][2]) return matrix[i][0];
+
+  for(i=0; i<3; i++) /* verifica as colunas */
+    if(matrix[0][i]==matrix[1][i] &&
+       matrix[0][i]==matrix[2][i]) return matrix[0][i];
+
+  /* testa as diagonais */
+  if(matrix[0][0]==matrix[1][1] &&
+     matrix[1][1]==matrix[2][2])
+       return matrix[0][0];
+
+  if(matrix[0][2]==matrix[1][1] &&
+     matrix[1][1]==matrix[2][0])
+       return matrix[0][2];
+  return ' ';
+}
