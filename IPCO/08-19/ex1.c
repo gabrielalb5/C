8bbef0274d6bc83bba31bec49e6784a168551a40
gabrielalb5 <gabrielalbino28@gmail.com>
2024-08-20 13:40:05 -0300

    .

--- IPCO/08-19/ex1.c
+++ IPCO/08-19/ex1.c
@@ -1,3 +1,15 @@
+/*Exercício 1. Faça um programa em C que gerencia as contas de clientes em um Banco fictício. Cada
+conta deverá conter o Número da conta, Nome do cliente e Saldo. Elabore um menu com as
+opções de Cadastramento da conta, Depósito, Saque, Consulta e Sair do programa.
+Observações:
+• Não será permitido cadastramento de contas com o mesmo número, o saque de
+valores superiores ao saldo e depósitos com valor negativo.
+• A consulta pode ser realizada pelo número da conta ou pelo nome do cliente.
+• Um cliente pode possuir mais de uma conta.
+• Considere que o banco tem no máximo 10 contas.
+Atenção: utilize o conceito de registro (struct) e funções para refinar, organizar e otimizar seu
+código.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX_CONTA 10
@@ -9,16 +21,16 @@ typedef struct{
     float saldo; //saldo da conta
 } Conta; // Define o nome do tipo criado
 
-Conta cadastrarConta(Conta contas[], int qteContas);
-int deposito(Conta contas[], int qteContas);
-int saque(Conta contas[], int qteContas);
-int consulta(Conta contas[], int qteContas);
+void cadastrarConta(Conta contas[], int qteContas);
+void deposito(Conta contas[], int qteContas);
+void saque(Conta contas[], int qteContas);
+void consulta(Conta contas[], int qteContas);
 
 int main(){
     int opcao, qteContas=0;
     Conta contas[MAX_CONTA];
     do{
-        printf("\nMenu");
+        printf("\nMENU");
         printf("\n1. Cadastrar conta");
         printf("\n2. Depositar");
         printf("\n3. Sacar");
@@ -27,11 +39,12 @@ int main(){
         printf("\nEscolha uma opção: ");
         scanf("%d",&opcao);
         if(opcao == 1){
-            if(qteContas <= MAX_CONTA){
+            if(qteContas < MAX_CONTA){
                 cadastrarConta(contas,qteContas);
                 qteContas++;
-            }else
+            }else{
                 printf("\nLimite de contas atingido");
+            }
         }
         if(opcao == 2) deposito(contas,qteContas);
         if(opcao == 3) saque(contas,qteContas);
@@ -41,30 +54,111 @@ int main(){
    return 0;
 }
 
-Conta cadastrarConta(Conta contas[], int qteContas){
+void cadastrarConta(Conta contas[], int qteContas){
     Conta c;
-    printf("\n\nFunção cadastrar conta");
+    printf("\nCADASTRO DE CONTA");
     printf("\nNúmero da conta: ");
     scanf("%d",&c.numero);
     for(int i=0;i<qteContas;i++){
         if(contas[i].numero==c.numero){
-            printf("\nNúmero da conta já cadastrado!\n");
+            printf("\nERRO: Número da conta já cadastrado!\n");
             return;
         }
     }
-    printf("\nNome: ");
+    printf("Nome: ");
     scanf("%s",c.nome);
-    printf("\nSaldo: R$");
+    printf("Saldo: R$");
     scanf("%f",&c.saldo);
     contas[qteContas] = c;
     printf("\nConta cadastrada com sucesso!\n");
 }
-int deposito(Conta contas[], int qteContas){
-    printf("função deposito");
+void deposito(Conta contas[], int qteContas){
+    float deposito;
+    int num_conta, pos=-1;
+    printf("\nDEPÓSITO");
+    printf("\nDigite o número da sua conta: ");
+    scanf("%d",&num_conta);
+    for(int i=0;i<qteContas;i++){
+        if(contas[i].numero==num_conta){
+          pos = i;  
+        }
+    }
+    if(pos==-1){
+        printf("ERRO: Conta não encontrada\n");
+        return;
+    }
+    printf("Quanto deseja depositar? R$");
+    scanf("%f",&deposito);
+    if(deposito>0){
+        contas[pos].saldo += deposito;
+    }else{
+        do{
+            printf("\nVocê digitou um valor negativo. Tente novamente!\nQuanto deseja depositar? R$");
+            scanf("%f",&deposito);
+        }while(deposito<=0);
+    }
+    printf("Depósito de R$%.2f realizado com sucesso!\n",deposito);
 }
-int saque(Conta contas[], int qteContas){
-    printf("função saque");
+void saque(Conta contas[], int qteContas){
+    float valor_saque;
+    int num_conta, pos=-1;
+    printf("\nSAQUE");
+    printf("\nDigite o número da sua conta: ");
+    scanf("%d",&num_conta);
+    for(int i=0;i<qteContas;i++){
+        if(contas[i].numero==num_conta){
+          pos = i;  
+        }
+    }
+    if(pos==-1){
+        printf("ERRO: Conta não encontrada\n");
+        return;
+    }
+    if(contas[pos].saldo == 0){
+        printf("ERRO: Seu saldo é de R$%.2f\n",contas[pos].saldo);
+        return;
+    }
+    do {
+        printf("Saldo de R$%.2f. Quanto deseja sacar? R$",contas[pos].saldo);
+        scanf("%f", &valor_saque);
+        if (valor_saque <= 0) {
+            printf("ERRO: O valor do saque deve ser positivo!\n");
+        }else if (valor_saque > contas[pos].saldo) {
+            printf("ERRO: Valor do saque maior que o saldo. Tente novamente!\n");
+        }
+    } while (valor_saque <= 0 || valor_saque > contas[pos].saldo);
+    contas[pos].saldo -= valor_saque;
+    printf("Saque de R$%.2f realizado com sucesso!\n",valor_saque);
 }
-int consulta(Conta contas[], int qteContas){
-    printf("função consulta");
+void consulta(Conta contas[], int qteContas){
+    int opcao, num_conta;
+    char nome;
+    do{
+        printf("\nCONSULTA");
+        printf("\n1. Pelo número da conta");
+        printf("\n2. Pelo nome");
+        printf("\nEscolha uma opção: ");
+        scanf("%d",&opcao);
+        if(opcao!=1 && opcao!=2){
+            printf("\nOpção inválida.");
+        }
+    }while(opcao!=1 && opcao!=2);
+    if(opcao==1){
+        printf("\nQual o número da conta? ");
+        scanf("%d",&num_conta);
+        for(int i=0;i<qteContas;i++){
+            if(contas[i].numero==num_conta){
+                printf("Seu saldo é de R$%.2f\n",contas[i].numero);
+            }
+        }
+    }else if(opcao==2){
+        printf("\nQual o seu nome? ");
+        scanf("%s",nome);
+        for(int i=0;i<qteContas;i++){
+            printf("Saldo das suas contas\n");
+            if(contas[i].nome==nome){
+                printf("R$%.2f\n",contas[i].numero);
+            }
+        }
+    }
 }
